f3ef5450edd840db5aca56fd7041c3409bdd405d
sitlle <Vktrt@yandex.ru>
2024-10-11 01:07:54 +0300

    Car has speed

--- backend/src/Cars/Cars.cpp
+++ backend/src/Cars/Cars.cpp
@@ -8,16 +8,16 @@ namespace Cars {
             // update position without turn to left and right
             switch (car.car_direction) {
                 case CarSettings::Direction::Up:
-                    car.car_settings.position_y++;
+                    car.car_settings.position_y -= car.car_settings.speed;
                     break;
                 case CarSettings::Direction::Down:
-                    car.car_settings.position_y--;
+                    car.car_settings.position_y += car.car_settings.speed;
                     break;
                 case CarSettings::Direction::Left:
-                    car.car_settings.position_x--;
+                    car.car_settings.position_x -= car.car_settings.speed;
                     break;
                 case CarSettings::Direction::Right:
-                    car.car_settings.position_x++;
+                    car.car_settings.position_x += car.car_settings.speed;
                     break;
             }
         }
