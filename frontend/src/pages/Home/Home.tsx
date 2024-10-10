6a22264112e27e9f08efecad94b2732d6084d0ef
PaRat07 <pasha.at.phone@gmail.com>
2024-10-10 17:21:34 +0300

    it works

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,35 +1,91 @@
 import {Stack, ToggleButton, ToggleButtonGroup, Typography} from '@mui/material';
 import React, {useEffect, useState} from "react";
 import CheckIcon from '@mui/icons-material/Check';
+import crossroadPath from '../../assets/crossroad.jpg'
+import carPath from '../../assets/car.webp'
 
+const throttle = (fn: Function, wait: number = 300) => {
+    let inThrottle: boolean,
+      lastFn: ReturnType<typeof setTimeout>,
+      lastTime: number;
+    return function (this: any) {
+      const context = this,
+        args = arguments;
+      if (!inThrottle) {
+        fn.apply(context, args);
+        lastTime = Date.now();
+        inThrottle = true;
+      } else {
+        clearTimeout(lastFn);
+        lastFn = setTimeout(() => {
+          if (Date.now() - lastTime >= wait) {
+            fn.apply(context, args);
+            lastTime = Date.now();
+          }
+        }, Math.max(wait - (Date.now() - lastTime), 0));
+      }
+    };
+  };
+
+interface Pos {
+    x: number,
+    y: number
+}
+
+interface Car {
+    pos: Pos,
+    direction: number,
+}
+
+interface Response {
+    cars: Car,
+    left_up_light_color: "red" | "yellow" | "green",
+    left_down_light_color: "red" | "yellow" | "green",
+    right_up_light_color: "red" | "yellow" | "green",
+    right_down_light_color: "red" | "yellow" | "green"
+}
 
 const Home = () => {
     const [canvas, setCanvas] = useState<HTMLCanvasElement>();
 
-    useEffect(() => {
+    const redrawRaw = async () => {
         if (canvas == null) return;
         const ctx = canvas.getContext("2d");
-        const landscape = new Image();
-        landscape.src = "/crossroad.jpg";
-        const car = new Image();
-        car.src = "/car.webp";
-        let pos = 0;
-        const loop = () => {
-            ctx.clearRect(0, 0, 800, 600);
-            ctx.drawImage(landscape, 0, 0);
-            ctx.drawImage(car, pos, pos, car.width / 10, car.height / 10);
-            ctx.stroke()
-            pos += 1;
-            if (pos < 500) {
-                requestAnimationFrame(loop)
-            }
-        }
-        requestAnimationFrame(loop);
-        return () => {};
-    }, [canvas]);
+        if (ctx === null) return;
+        console.log("Redraw is called");
+        
+        const crossroadImage = new Image();
+        crossroadImage.src = crossroadPath;
+        const carImage = new Image();
+        carImage.src = carPath;
+        const resp = (await (await fetch("http://localhost:1234/cars")).json()) as Response;
+        ctx.clearRect(0, 0, 800, 600);
+        ctx.drawImage(crossroadImage, 0, 0);
+        ctx.drawImage(carImage, resp.cars.pos.x, resp.cars.pos.y, carImage.width / 10, carImage.height / 10);
+        ctx.fillStyle = resp.left_up_light_color;
+        ctx.beginPath();
+        ctx.arc(0, 0, 20, 0, 2 * Math.PI);
+        ctx.fill()
+        ctx.fillStyle = resp.left_down_light_color;
+        ctx.beginPath();
+        ctx.arc(0, 300, 20, 0, 2 * Math.PI);
+        ctx.fill()
+        ctx.fillStyle = resp.right_down_light_color;
+        ctx.beginPath();
+        ctx.arc(600, 300, 20, 0, 2 * Math.PI);
+        ctx.fill()
+        ctx.fillStyle = resp.right_up_light_color;
+        ctx.beginPath();
+        ctx.arc(500, 0, 20, 0, 2 * Math.PI);
+        ctx.fill()
+        requestAnimationFrame(redraw);
+    };
+    const redraw = throttle(redrawRaw, 0);
+    requestAnimationFrame(redraw);
+
     return (
         <>
-            <canvas ref={ setCanvas } width={ 800 } height={ 600 }></canvas>
+            <canvas ref={ setCanvas } width={ 800 } height={ 600 } />
         </>
     );
 };
