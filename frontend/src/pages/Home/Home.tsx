cb697d029986dd195a74bd3de41677464b7896bd
sitlle <Vktrt@yandex.ru>
2024-10-11 10:41:26 +0300

    draw cars array

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,31 +1,31 @@
 import {Stack, ToggleButton, ToggleButtonGroup, Typography} from '@mui/material';
 import React, {useEffect, useState} from "react";
 import CheckIcon from '@mui/icons-material/Check';
-import crossroadPath from '../../assets/crossroad.jpg'
+import crossroadPath from '../../assets/BigCrossRoad.jpeg'
 import carPath from '../../assets/Car.png'
 
 const throttle = (fn: Function, wait: number = 300) => {
     let inThrottle: boolean,
-      lastFn: ReturnType<typeof setTimeout>,
-      lastTime: number;
+        lastFn: ReturnType<typeof setTimeout>,
+        lastTime: number;
     return function (this: any) {
-      const context = this,
-        args = arguments;
-      if (!inThrottle) {
-        fn.apply(context, args);
-        lastTime = Date.now();
-        inThrottle = true;
-      } else {
-        clearTimeout(lastFn);
-        lastFn = setTimeout(() => {
-          if (Date.now() - lastTime >= wait) {
+        const context = this,
+            args = arguments;
+        if (!inThrottle) {
             fn.apply(context, args);
             lastTime = Date.now();
-          }
-        }, Math.max(wait - (Date.now() - lastTime), 0));
-      }
+            inThrottle = true;
+        } else {
+            clearTimeout(lastFn);
+            lastFn = setTimeout(() => {
+                if (Date.now() - lastTime >= wait) {
+                    fn.apply(context, args);
+                    lastTime = Date.now();
+                }
+            }, Math.max(wait - (Date.now() - lastTime), 0));
+        }
     };
-  };
+};
 
 interface Pos {
     x: number,
@@ -38,7 +38,7 @@ interface Car {
 }
 
 interface Response {
-    cars: Car,
+    cars: Car[],
     left_up_light_color: "red" | "yellow" | "green",
     left_down_light_color: "red" | "yellow" | "green",
     right_up_light_color: "red" | "yellow" | "green",
@@ -52,40 +52,49 @@ const Home = () => {
         if (canvas == null) return;
         const ctx = canvas.getContext("2d");
         if (ctx === null) return;
-        console.log("Redraw is called");
-        
         const crossroadImage = new Image();
         crossroadImage.src = crossroadPath;
         const carImage = new Image();
         carImage.src = carPath;
         const resp = (await (await fetch("http://localhost:1234/cars")).json()) as Response;
-        ctx.clearRect(0, 0, 800, 600);
+        ctx.clearRect(0, 0, 1498, 723);
         ctx.drawImage(crossroadImage, 0, 0);
-        ctx.drawImage(carImage, resp.cars.pos.x, resp.cars.pos.y, carImage.width / 4, carImage.height / 4);
+        resp.cars.map((car) => {
+            ctx.drawImage(carImage, car.pos.x, car.pos.y, carImage.width / 4, carImage.height / 4);
+        });
+        ctx.strokeStyle = "black";
         ctx.fillStyle = resp.left_up_light_color;
         ctx.beginPath();
-        ctx.arc(0, 0, 20, 0, 2 * Math.PI);
-        ctx.fill()
+        ctx.arc(250, 150, 20, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.arc(250, 150, 20, 0, 2 * Math.PI);
+        ctx.stroke();
         ctx.fillStyle = resp.left_down_light_color;
         ctx.beginPath();
-        ctx.arc(0, 300, 20, 0, 2 * Math.PI);
-        ctx.fill()
+        ctx.arc(250, 350, 20, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.arc(250, 350, 20, 0, 2 * Math.PI);
+        ctx.stroke();
         ctx.fillStyle = resp.right_down_light_color;
         ctx.beginPath();
-        ctx.arc(600, 300, 20, 0, 2 * Math.PI);
+        ctx.arc(450, 350, 20, 0, 2 * Math.PI);
         ctx.fill()
+        ctx.arc(450, 350, 20, 0, 2 * Math.PI);
+        ctx.stroke()
         ctx.fillStyle = resp.right_up_light_color;
         ctx.beginPath();
-        ctx.arc(500, 0, 20, 0, 2 * Math.PI);
+        ctx.arc(450, 150, 20, 0, 2 * Math.PI);
         ctx.fill()
-        requestAnimationFrame(redraw);
+        ctx.arc(450, 150, 20, 0, 2 * Math.PI);
+        ctx.stroke()
+        requestAnimationFrame(redrawRaw);
     };
-    const redraw = throttle(redrawRaw, 0);
-    requestAnimationFrame(redraw);
+    // const redraw = throttle(redrawRaw, 0);
+    requestAnimationFrame(redrawRaw);
 
     return (
         <>
-            <canvas ref={ setCanvas } width={ 800 } height={ 600 } />
+            <canvas ref={ setCanvas } width={ 1498 } height={ 723 } />
         </>
     );
 };
