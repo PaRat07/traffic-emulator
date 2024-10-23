57c9581297b91fa0917a43eb3f1e071751985ca9
sitlle <Vktrt@yandex.ru>
2024-10-23 13:13:28 +0300

    update light color

--- backend/src/TrafficLight/Light.cpp
+++ backend/src/TrafficLight/Light.cpp
@@ -1,12 +1,15 @@
 #include "../../TrafficLight/Light.h"
+#include "../../UserSettings/UserLightSettings.h"
 
 std::string Light::get_light_color() {
     if (light_color == LightSettings::Color::Red) {
         return "red";
-    } else if (light_color == LightSettings::Color::YellowUp ||
+    }
+    if (light_color == LightSettings::Color::YellowUp ||
     light_color == LightSettings::Color::YellowDown) {
         return "yellow";
-    } else if (light_color == LightSettings::Color::Green) {
+    }
+    if (light_color == LightSettings::Color::Green) {
         return "green";
     }
     return "green";
@@ -22,4 +25,23 @@ void Light::update_color() {
     } else if (light_color == LightSettings::Color::YellowUp) {
         light_color = LightSettings::Color::Green;
     }
+}
+
+int Light::get_light_time() {
+    if (UserLightSettings::user_mode == LightSettings::Mode::Static) {
+        if (light_color == LightSettings::Color::Red) {
+            return UserLightSettings::user_red_light_time;
+        }
+        if (light_color == LightSettings::Color::YellowDown ||
+        light_color == LightSettings::Color::YellowUp) {
+            return UserLightSettings::user_yellow_light_time;
+        }
+        if (light_color == LightSettings::Color::Green) {
+            return UserLightSettings::user_green_light_time;
+        }
+    }
+    if (UserLightSettings::user_mode == LightSettings::Mode::Dynamic) {
+        return 3; // add calc cars
+    }
+    return 3;
 }
