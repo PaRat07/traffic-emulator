8e51e5fec05ad1e57e1f68734f77737b2e1e37d6
sitlle <Vktrt@yandex.ru>
2024-10-23 20:44:19 +0300

    swap yellowup and yellow down

--- backend/src/TrafficLight/Light.cpp
+++ backend/src/TrafficLight/Light.cpp
@@ -17,12 +17,12 @@ std::string Light::get_light_color() {
 
 void Light::update_color() {
     if (light_color == LightSettings::Color::Green) {
-        light_color = LightSettings::Color::YellowDown;
-    } else if (light_color == LightSettings::Color::YellowDown) {
-        light_color = LightSettings::Color::Red;
-    } else if (light_color == LightSettings::Color::Red) {
         light_color = LightSettings::Color::YellowUp;
     } else if (light_color == LightSettings::Color::YellowUp) {
+        light_color = LightSettings::Color::Red;
+    } else if (light_color == LightSettings::Color::Red) {
+        light_color = LightSettings::Color::YellowDown;
+    } else if (light_color == LightSettings::Color::YellowDown) {
         light_color = LightSettings::Color::Green;
     }
 }
