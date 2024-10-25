69748348ee9e87105b30689fd6c9e7e8b3103950
sitlle <Vktrt@yandex.ru>
2024-10-25 09:31:13 +0300

    add dynamic mode

--- backend/src/UserSettings/UserLightSettings.cpp
+++ backend/src/UserSettings/UserLightSettings.cpp
@@ -1,7 +1,7 @@
 #include "../../UserSettings/UserLightSettings.h"
 
 namespace UserLightSettings {
-    LightSettings::Mode user_mode = LightSettings::Mode::Static;
+    LightSettings::Mode user_mode = LightSettings::Mode::Dynamic;
     int user_green_light_time = 5;
     int user_yellow_light_time = 2;
     int user_red_light_time = 5;
